06d8b81b57386865e0c2f818f096907fe623fff0
Zack <zackakil94@gmail.com>
2021-01-26 01:21:21 +0000

    Add files via upload

---
+++ button_components.js
@@ -0,0 +1,27 @@
+// define style rules to be programtically loaded
+var style = document.createElement('style');
+style.innerHTML = `
+
+.red { color: #F00; font-size: 30px; border: blue dashed 2px;}
+
+`;
+document.getElementsByTagName('head')[0].appendChild(style);
+
+
+// define component
+Vue.component('button-counter', {
+    props: ['title', 'items'],
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: `
+    <div class="red">
+        <button v-on:click="count++">You clicked me {{ count }} times. {{title}}</button>
+        <p v-for="item in items">{{item}}</p>
+    </div>
+    `
+})
+
+
---
+++ list_components.js
@@ -0,0 +1,26 @@
+// define style rules to be programtically loaded
+var style = document.createElement('style');
+style.innerHTML = `
+
+.list { color: blue; font-size: 20px; border: red solid 2px;}
+
+`;
+document.getElementsByTagName('head')[0].appendChild(style);
+
+
+// define component
+Vue.component('counter-list', {
+    props: ['items'],
+    data: function () {
+        return {
+            
+        }
+    },
+    template: `
+    <div class="list">
+        <p v-for="item in items">This is my item : {{item}}</p>
+    </div>
+    `
+})
+
+
